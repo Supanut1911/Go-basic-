925da5f16463f628483a1fe86cdd82a9f060e13d
Supanut1911 <ynotmela@gmail.com>
2023-05-05 17:57:10 +0700

    if condition

--- main.go
+++ main.go
@@ -8,15 +8,30 @@ var z int
 func main() {
 	//declaration
 	var x int = 10
-
+	_ = x
 
 	//short declaration
 	y := 10
+	_ = y
 
 	//default value
 	aa := z
+	_ = aa
 
 	// fmt.Printf("%d %d %f %d", x,y,z, aa)
-	fmt.Printf("%d %d %d %d", x,y,z, aa)
+	// fmt.Printf("%d %d %d %d", x,y,z, aa)
 
+	condition()
+
+}
+
+func condition() {
+	point := 10
+
+	if point >= 50 && point <= 100  {
+		fmt.Printf("Pass")
+	} else if point < 20 {
+		fmt.Printf("fail")
+
+	}
 }
