c570de94ea6bac6ee2d0b00121fd20da4d6f99ca
Supanut1911 <ynotmela@gmail.com>
2023-05-05 17:40:42 +0700

    about variable

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+//package scope / global var
+var z int
+
 func main() {
-	fmt.Printf("hello %T\n", true)
+	//declaration
+	var x int = 10
+
+
+	//short declaration
+	y := 10
+
+	//default value
+	aa := z
+
+	// fmt.Printf("%d %d %f %d", x,y,z, aa)
+	fmt.Printf("%d %d %d %d", x,y,z, aa)
+
 }
